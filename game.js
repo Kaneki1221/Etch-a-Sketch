5df918868c31043d9612bfc19ea7c3151ad65e79
Sumyth2107 <sumith2107@gmail.com>
2024-07-07 01:57:53 +0530

    Update game.js
    error fixed

--- game.js
+++ game.js
@@ -1,26 +1,28 @@
 function createGrid() {
-    var gridSize = parseInt(document.getElementById('gridSizeInput').value);
-    if (isNaN(gridSize) || gridSize <= 0) {
-      alert("Please enter a valid grid size (greater than 0).");
-      return;
-    }
+  var gridSize = parseInt(document.getElementById('gridSizeInput').value);
+  if (isNaN(gridSize) || gridSize <= 0) {
+    alert("Please enter a valid grid size (greater than 0).");
+    return;
+  }
+}
+var gridSize=16;
 
 
 for (var i = 0; i < gridSize; i++) {
-    for (var j = 0; j < gridSize; j++) {
-        var newDiv = document.createElement('div');
-        newDiv.className = "mydiv";
-        var container = document.getElementById('container');
-        container.appendChild(newDiv);
-        
-        
-        
-    }
+  for (var j = 0; j < gridSize; j++) {
+      var newDiv = document.createElement('div');
+      newDiv.className = "mydiv";
+      var container = document.getElementById('container');
+      container.appendChild(newDiv);
+     
+      
+      
+      
+  }
 }
 document.querySelectorAll('.mydiv').forEach(item => {
-    item.addEventListener('mouseenter', event => {
-      const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-      event.target.style.backgroundColor = randomColor;
-    });
+  item.addEventListener('mouseenter', event => {
+    const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    event.target.style.backgroundColor = randomColor;
   });
-  
+});
