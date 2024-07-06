8f7ff90f8018286209d3e76de0a39771ba82d224
Sumyth2107 <sumith2107@gmail.com>
2024-07-06 19:46:54 +0530

    added css ,html ,js and removed test borders

--- game.js
+++ game.js
@@ -4,6 +4,13 @@ for (var i = 0; i < 16; i++) {
         newDiv.className = "mydiv";
         var container = document.getElementById('container');
         container.appendChild(newDiv);
-        newDiv.innerText=j;
+        
     }
 }
+document.querySelectorAll('.mydiv').forEach(item => {
+    item.addEventListener('mouseenter', event => {
+      const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+      event.target.style.backgroundColor = randomColor;
+    });
+  });
+  
